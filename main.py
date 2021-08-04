32512345a8fa10eeda761fb3338ddcf23603752e
Oleksandra Mala <oleksandra.s.mala@gmail.com>
2021-08-04 18:59:04 +0300

    Third commit

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import random
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
-block_size = 30
-left_margin = 40
-upper_margin = 50
+block_size = 40
+left_margin = 50
+upper_margin = 60
 
 size = (left_margin + 30 * block_size, upper_margin + 15 * block_size)
 
@@ -15,13 +15,15 @@ pygame.init()
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Okręgi")
 
-font_size = int(block_size/1.5)
+font_size = int(block_size / 1.5)
+
 font = pygame.font.SysFont('notosans', font_size)
 
 class ShipsOnGrid:
     def __init__(self):
         self.available_blocks = set((a, b) for a in range(1, 11) for b in range(1, 11))
         self.ships_set = set()
+        self.ships = self.populate_grid()
 
     def create_start_block(self, available_blocks):
         x_or_y = random.randint(0, 1)
@@ -29,9 +31,9 @@ class ShipsOnGrid:
         x, y = random.choice(tuple(available_blocks))
         return x, y, x_or_y, str_rev
 
-    def create_ship(self, number_of_blocks, avaibable_blocks):
+    def create_ship(self, number_of_blocks, available_blocks):
         ship_coordinates = []
-        x, y, x_or_y, str_rev = self.create_start_block(avaibable_blocks)
+        x, y, x_or_y, str_rev = self.create_start_block(available_blocks)
         for _ in range(number_of_blocks):
             ship_coordinates.append((x, y))
             if not x_or_y:
@@ -40,7 +42,7 @@ class ShipsOnGrid:
                 str_rev, y = self.add_block_to_ship(y, str_rev, x_or_y, ship_coordinates)
         if self.if_ship_valid(ship_coordinates):
             return ship_coordinates
-        return self.create_ship(number_of_blocks, avaibable_blocks)
+        return self.create_ship(number_of_blocks, available_blocks)
 
     def add_block_to_ship(self, coor, str_rev, x_or_y, ship_coordinates):
 
@@ -50,10 +52,52 @@ class ShipsOnGrid:
         else:
             return str_rev, ship_coordinates[-1][x_or_y] + str_rev
 
-
-    def is_ship_valid(self, new_ship):
+    def if_ship_valid(self, new_ship):
         ship = set(new_ship)
         return ship.issubset(self.available_blocks)
+
+    def add_new_ship_to_set(self, new_ship):
+        for elem in new_ship:
+            self.ships_set.add(elem)
+
+    def update_available_blocks_for_creating_ships(self, new_ship):
+        for elem in new_ship:
+            for k in range(-1, 2):
+                for m in range(-1, 2):
+                    if 0 < (elem[0] + k) < 11 and 0 < (elem[1] + m) < 11:
+                        self.available_blocks.discard((elem[0] + k, elem[1] + m))
+
+    def populate_grid(self):
+        ships_coordinates_list = []
+        for number_of_blocks in range(4, 0, -1):
+            for _ in range(5 - number_of_blocks):
+                new_ship = self.create_ship(number_of_blocks, self.available_blocks)
+                ships_coordinates_list.append(new_ship)
+                self.add_new_ship_to_set(new_ship)
+                self.update_available_blocks_for_creating_ships(new_ship)
+        return ships_coordinates_list
+
+computer = ShipsOnGrid()
+human = ShipsOnGrid()
+
+def draw_ships(ships_coordinates_list):
+    for elem in ships_coordinates_list:
+        ship = sorted(elem)
+        x_start = ship[0][0]
+        y_start = ship[0][1]
+        #vert
+        if len(ship) > 1 and ship[0][0] == ship[1][0]:
+            ship_width = block_size
+            ship_height = block_size * len(ship)
+        #hor and 1block
+        else:
+            ship_width = block_size * len(ship)
+            ship_height = block_size
+        x = block_size * (x_start - 1) + left_margin
+        y = block_size * (y_start - 1) + upper_margin
+        if ships_coordinates_list == human.ships:
+            x += 15 * block_size
+        pygame.draw.rect(screen, BLACK, ((x, y), (ship_width, ship_height)), width=block_size//10)
 
 
 
@@ -64,7 +108,7 @@ def draw_grid():
 
         #Hor grid1
         pygame.draw.line(screen, BLACK, (left_margin, upper_margin+i*block_size),
-                         (left_margin+10*block_size,upper_margin+i*block_size), 1)
+                         (left_margin+10*block_size, upper_margin+i*block_size), 1)
         #Vert grid1
         pygame.draw.line(screen, BLACK, (left_margin+i*block_size, upper_margin),
                          (left_margin+i*block_size, upper_margin+10*block_size), 1)
@@ -72,8 +116,8 @@ def draw_grid():
         pygame.draw.line(screen, BLACK, (left_margin+15*block_size, upper_margin + i * block_size),
                          (left_margin + 25 * block_size, upper_margin + i * block_size), 1)
         # Vert grid2
-        pygame.draw.line(screen, BLACK, (left_margin + i * block_size+15*block_size, upper_margin),
-                         (left_margin + i * block_size+15*block_size, upper_margin + 10 * block_size), 1)
+        pygame.draw.line(screen, BLACK, (left_margin + (i + 15) * block_size, upper_margin),
+                         (left_margin + (i + 15) * block_size, upper_margin + 10 * block_size), 1)
 
         if i < 10:
             num_vert = font.render(str(i+1), True, BLACK)
@@ -87,7 +131,7 @@ def draw_grid():
             screen.blit(num_vert, (left_margin - (block_size//2+num_vert_width//2),
                                    upper_margin + i*block_size + (block_size//2 - num_vert_height//2)))
             #Hor letters grid1
-            screen.blit(letters_hor, (left_margin + i*block_size + (block_size//2 - letters_hor_width//2),
+            screen.blit(letters_hor, (left_margin + i * block_size + (block_size//2 - letters_hor_width//2),
                                       upper_margin + 10 * block_size))
 
             # Vert num grid2
@@ -100,12 +144,17 @@ def draw_grid():
 def main():
     game_over = False
     screen.fill(WHITE)
+    draw_grid()
+    draw_ships(computer.ships)
+    draw_ships(human.ships)
+    pygame.display.update()
+
     while not game_over:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_over = True
-        draw_grid()
-        pygame.display.update()
+
+
 
 main()
 pygame.quit()
